52527c5db797c38fa386351aa1f2205e7db944e4
edoalemp <eduardo.munoz.p@gmail.com>
2019-08-02 00:21:24 +0000

    tabla organization

--- src/models.py
+++ src/models.py
@@ -2,10 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(80), unique=True, nullable=False)
+    phone = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    persons = db.relationship('Address', backref='person')
+
+    def __repr__(self):
+        return '<Organization %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "email": self.email
+        }
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'),
+        nullable=False)
 
     def __repr__(self):
         return '<Person %r>' % self.username
